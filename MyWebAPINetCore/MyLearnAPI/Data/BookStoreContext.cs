6f5d26624bf27169d7be7031767f8f8b47b6b54d
Nguyen Hoang Khai <nguyenhoangkhai2003tgl@gmail.com>
2023-05-06 10:39:30 +0700

    Xong bài 1: Khởi tạo project api đầu tiền_ MyBookStore

---
+++ MyWebAPINetCore/MyLearnAPI/Data/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyLearnAPI.Data;
+
+public class BookStoreContext : DbContext
+{
+    public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) 
+    {
+    }
+
+    // tao moi quan he
+    #region DbSet
+    public DbSet<Book> Books { get; set; }
+    #endregion
+}
--- MyWebAPINetCore/MyLearnAPI/Program.cs
+++ MyWebAPINetCore/MyLearnAPI/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLearnAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// giúp cho chia sẻ api cho mọi thiết bị, mọi nền tảng
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyHeader()));
+
+// khai bao chuoi ket noi 
+builder.Services.AddDbContext<BookStoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookStore"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
